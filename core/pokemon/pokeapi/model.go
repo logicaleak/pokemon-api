c869f9517146189991cabb82a131a041053a0170
oz <safaogluozum@gmail.com>
2020-11-07 20:02:28 +0000

    Lint fixes

--- core/pokemon/pokeapi/model.go
+++ core/pokemon/pokeapi/model.go
@@ -1,9 +1,11 @@
 package pokeapi
 
+// PokemonSpecies is the returned model from the species endpoint
 type PokemonSpecies struct {
-	FlavourTextEntries []FlavourTextEntries `json:"flavor_text_entries`
+	FlavourTextEntries []FlavourTextEntries `json:"flavor_text_entries"`
 }
 
+// FlavourTextEntries is the description entries for the pokemon
 type FlavourTextEntries struct {
 	FlavorText string `json:"flavor_text"`
 }
