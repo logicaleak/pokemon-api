65eac833f39128d325fa38b8e82dc9011957ec60
Ozum Safaoglu <safaogluozum@gmail.com>
2020-11-08 19:38:15 +0000

    Tooling work and an implementation bit, UNSTABLE

--- api/swagger/implementation/pokemon.go
+++ api/swagger/implementation/pokemon.go
@@ -1,19 +1,44 @@
 package implementation
 
 import (
+	"context"
+	"time"
+
 	"github.com/go-openapi/runtime/middleware"
 	"ozum.safaoglu/pokemon-api/api/swagger/restapi/operations/pokemons"
+	"ozum.safaoglu/pokemon-api/cache"
+	"ozum.safaoglu/pokemon-api/config"
+	"ozum.safaoglu/pokemon-api/core/pokemon/pokeapi"
+	"ozum.safaoglu/pokemon-api/core/service"
+	"ozum.safaoglu/pokemon-api/core/shakespeare"
 )
 
 type ShakespeareanPokemonAPI struct {
+	spService service.ShakespeareanPokemon
 }
 
 func NewShakespeareanPokemonAPI() *ShakespeareanPokemonAPI {
-	return nil
+	pokeAPI := pokeapi.NewPokeAPI(config.CFG.PokeapiBaseURL)
+	spClient := shakespeare.NewClient(config.CFG.ShakespeareBaseURL)
+
+	cache, err := cache.NewRedis(config.CFG.RedisAddr, "")
+	if err != nil {
+		panic(err)
+	}
+	cachedPokeAPI := pokeapi.NewCachedPokeAPI(pokeAPI, cache)
+	cachedSPClient := shakespeare.NewCachedSPClient(spClient, cache)
+
+	&ShakespeareanPokemonAPI{
+		spService: service.NewShakespeareanPokemon(cachedPokeAPI, cachedSPClient, "en")
+	} 
 }
 
 // GetPokemon implements the swagger api endpoint
 // It returns the description of the passed pokemon in shakespearean English
 func (s *ShakespeareanPokemonAPI) GetPokemon(params pokemons.GetParams) middleware.Responder {
+	// ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	// defer cancel()
+	// s.spService.GetDescription(ctx, params)
+
 	return middleware.NotImplemented("operation pokemons.Get has not yet been implemented")
 }
--- config/config.go
+++ config/config.go
@@ -1,7 +1,22 @@
 package config
 
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
 // Config is the app configuration object
 type Config struct {
 	PokeapiBaseURL     string
 	ShakespeareBaseURL string
+	RedisAddr          string
+}
+
+// CFG Global config of the app
+var CFG *Config
+
+func init() {
+	err := envconfig.Process("pokemonapi", CFG)
+	if err != nil {
+		panic(err)
+	}
 }
